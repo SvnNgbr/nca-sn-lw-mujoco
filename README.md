dcb39fb712095b9014d1fc4bd915d126e0edc406
“SvnNgbr” <Neugebauer.sven@t-online.de>
2026-06-25 17:08:41 +0200

    Added Setup and needed docs

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # nca-sn-lw-mujoco
-Neuromorphic Control Assignment für den SoSe Kurs von Dr. rer. nat. Johannes Maria Leugering
+Neuromorphic Control Assignment für den SoSe Kurs von Dr. rer. nat. Johannes Maria Leugering for Introduction to Neuromorphic Control
+
+We used the mujoco repository at LINK and build upon it.
+
+---
+
+# Getting Started
+Clone the Repository
+
+```bash
+git clone https://github.com/SvnNgbr/nca-sn-lw-mujoco
+```
+
+Set up and activate a python environment
+
+```bash
+conda create -n mujoco_3_11_15 python=3.11.15
+conda activate mujoco_3_11_15
+```
+**--- OR ---** 
+```bash
+python -m venv /path/to/new/virtual/environment
+source /path/to/new/virtual/environment/bin/activate
+```
+Navigate to the repository folder
+```bash
+cd nca-sn-lw-mujoco
+```
+
+And execute setup script.
+This will install dependencies, clone the Mujoco repository inside the nca-sn-lw-mujoco repository and modify and copy files.
+Executing this is **neccessary** for the main script to work.
+```bash
+python setup.py
+```
+
+If you have a **blackwell-series GPU**, which is not yet supported by the stable torch, you might need to install the [nightly torch version](https://pytorch.org/get-started/locally/). 
+The setup script informs you in the last couple of lines if that is applicable for you.
+We used the following command:
+```bash
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu129
+```
+
+You should now be all set to execute the main programs for each task!
+
+---
---
+++ setup.py
@@ -0,0 +1,106 @@
+import subprocess
+import os
+import sys
+
+def ask_user_for_confirmation(prompt: str) -> bool:
+    while True:
+        user_input = input(prompt).strip().lower()
+        if user_input == "y":
+            return True
+        elif user_input == "n":
+            return False
+        else:
+            print("Invalid input. please use only 'y' or 'n'.")
+
+def in_conda() -> bool:
+    return "conda" in sys.executable
+
+def in_venv() -> bool:
+    # Prüfen, ob das Skript in einer virtuellen Umgebung läuft
+    return hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix)
+
+if __name__ == "__main__":
+
+    if in_conda():
+        print("Detected conda environment:", os.environ.get("CONDA_DEFAULT_ENV"))
+    elif in_venv():
+        print("Detected virtual environment:", sys.prefix)
+    else:
+        print("No conda environment detected. Aborting...")
+        raise EnvironmentError()
+    
+    print("Starting Setup...")
+    
+    mujocopath = os.path.join(
+        os.path.dirname(__file__),
+        "mujoco"
+    )
+
+    if not os.path.exists(mujocopath):
+        print("Cloning the MuJoCo repository...")
+        subprocess.check_call(
+            [
+                "git",
+                "clone",
+                "https://github.com/deepmind/mujoco.git"
+            ],
+            cwd = os.path.dirname(__file__)
+        )
+
+        print("Done!")
+
+    else:
+        print("MuJoCo repository already cloned or folder exists. Skipping...")
+
+    # Dummy variable for init reasons
+    blackwell = False
+    print("Installing dependencies...")
+    if ask_user_for_confirmation("Blackwell GPU? (y/n): "):
+        subprocess.check_call(
+            [
+                sys.executable, 
+                "-m", 
+                "pip", 
+                "install", 
+                "-r", 
+                os.path.join(
+                    os.path.dirname(__file__), 
+                    "requirements_blackwell.txt"
+                )
+            ]
+        )
+        blackwell = True
+        # subprocess.check_call(
+        #     [
+        #         sys.executable,
+        #         "-m",
+        #         "pip",
+        #         "install",
+        #         "torch",
+        #         "torchvision",
+        #         "torchaudio",
+        #         "--index-url",
+        #         "https://download.pytorch.org/whl/cu129",
+        #     ]
+        # )
+    else:
+        subprocess.check_call(
+            [
+                sys.executable, 
+                "-m", 
+                "pip", 
+                "install", 
+                "-r", 
+                os.path.join(
+                    os.path.dirname(__file__), 
+                    "requirements.txt"
+                )
+            ]
+        )
+    print("Done!")
+
+    if blackwell:
+        print("Torch installation for Blackwell-series GPUs not possoble in script, please refer to: [link to documentation].")
+        print("Probably like this: 'pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu129'")
+
+    print("Setup complete!")
